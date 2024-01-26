cbff7a47cab4957b448af1a00730b30919e718e6
Joyce den Hertog <joycedenhertog2@live.nl>
2024-01-26 19:01:44 +0100

    bug in opt, edit generate.py

--- data_loaders/humanml/utils/get_opt.py
+++ data_loaders/humanml/utils/get_opt.py
@@ -69,7 +69,7 @@ def get_opt(opt_path, device):
         opt.dim_pose = 251
         opt.max_motion_length = 196
     elif opt.dataset_name == 'swdance':
-        opt.data_root = './dataset/SWDance2'
+        opt.data_root = './dataset/SWDance'
         opt.motion_dir = pjoin(opt.data_root, 'new_joint_vecs')
         opt.text_dir = pjoin(opt.data_root, 'texts')
         opt.joints_num = 22
