c8b0cde7816ef3653740485da7dded50d1d793de
Joyce den Hertog <joycedenhertog@live.nl>
2024-01-28 17:17:27 +0100

    add train/val split notebook + some minor changes

--- sample/generate.sh
+++ sample/generate.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
 #SBATCH --export=ALL
 #SBATCH --job-name=generate-mdm
-#SBATCH --mail-user=yourname@email.com  #TODO: fill in correct email
+#SBATCH --mail-user=user@email.com #TODO: fill in your correct email
 #SBATCH --mail-type="ALL"
 #SBATCH --time=00:40:00
 #SBATCH --partition=gpu-short
-#SBATCH --output=/path/to/your/logs/generate/%x_%j.out  #TODO: make correct location
+#SBATCH --output=/home/motion-diffusion-model/save/logs/%x_%j.out  #TODO: fill in yourß correct location
 #SBATCH --gres=gpu:1
 
 echo "## Starting MDM generating on $HOSTNAME at $(pwd)"
 
-source /path/to/your/env/bin/activate       #TODO: load your own env and modules you need. 
+source $HOME/data1/thesis-env/bin/activate      #TODO: load your own env and modules you need. 
 echo "## using python:"
 which python
 export PYTHONPATH=".":$PYTHONPATH
 
-model=/motion-diffusion-model/models/model000535000.pt
-output_dir=motion-diffusion-model/save/swdance/samples
+
+
+model=$HOME/motion-diffusion-model/save/swdance/model000535000.pt
+output_dir=$HOME/motion-diffusion-model/save/swdance/samples
 
 # ------ GENERATE FROM TEXTFILE ------ # 
-textfile=/motion-diffusion-model/sample/texts.txt        
+textfile=$HOME/motion-diffusion-model/sample/val_texts.txt        
 
 echo "Generating for file: $textfile and model: $model"
 python -m sample.generate \
@@ -32,24 +34,24 @@ python -m sample.generate \
 
 
 # ------- GENERATE FROM BELOW DEFINED PROMPTS ------ # 
-declare -a prompts=(
-    "When do we draw the line? joy delight happiness passion excitement love"
-    "When do we draw the line?"
-)
+# declare -a prompts=(
+#     "When do we draw the line? joy delight happiness passion excitement love"
+#     "When do we draw the line?"
+# )
 
-for prompt in "${prompts[@]}"
-do
-    echo "Generating for prompt: $prompt"
-    python -m sample.generate \
-        --model_path $model \
-        --text_prompt "$prompt" \
-        --num_repetitions 1 \
-        --output_dir $output_dir 
+# for prompt in "${prompts[@]}"
+# do
+#     echo "Generating for prompt: $prompt"
+#     python -m sample.generate \
+#         --model_path $model \
+#         --text_prompt "$prompt" \
+#         --num_repetitions 1 \
+#         --output_dir $output_dir 
 
-    echo "done with prompt $prompt"
-done
+#     echo "done with prompt $prompt"
+# done
 
-echo "done generating!" 
+# echo "done generating!" 
 
 
 
